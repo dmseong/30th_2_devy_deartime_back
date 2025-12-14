014ac2dbae75593de2b547908384c2c4870688a1
dmseong <149465698+dmseong@users.noreply.github.com>
2025-12-15 03:37:10 +0900

    init: 의존성 설정

--- src/main/java/com/project/deartime/DeartimeApplication.java
+++ src/main/java/com/project/deartime/DeartimeApplication.java
@@ -2,8 +2,10 @@ package com.project.deartime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DeartimeApplication {
 
 	public static void main(String[] args) {
